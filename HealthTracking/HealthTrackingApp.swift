abe27883fcf78a948a82c6ba04bf7b12d1a9417c
doris <doris070714@gmail.com>
2024-01-12 14:29:30 +0800

    Initial Commit

---
+++ HealthTracking/HealthTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackingApp.swift
+//  HealthTracking
+//
+//  Created by Doris Wen on 2024/1/12.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
